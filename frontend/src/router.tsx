5b001304403e45d0c09d470d41be816f6b897835
Kevin Chen <kevincyq@gmail.com>
2025-10-02 23:46:55 +0800

    Update some config

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import Home from './pages/Home/index';  // 导入功能完整的Home组件
 import CollectionDetail from './pages/Collection/Detail';
 
+//路由配置
 export const router = createBrowserRouter([
   {
     path: '/',
